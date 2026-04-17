c8d4a9164333cb0b1aa5cead6a523a4f07915ccc
Elsp3aweyDev <elspaey9@gmail.com>
2026-04-17 16:52:19 +0200

    routing & constants

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
 
+const knumberOfPadding = 20;
+
 class Constants {
+  // constants of colors
   static const Color blackColor = Colors.black;
   static const Color greyColor = Colors.grey;
+  static const Color kPrimaryOrange = Color(0xFFFF6B2C);
+  static const Color kAccentYellow = Color(0xFFFDBB2D);
+  static const Color kPureBlackBG = Color(0xFF000000);
+  static const Color kCardNavy = Color(0xFF121212);
+  static const Color kPureWhite = Color(0xFFFFFFFF);
+  static const Color kTextSecondary = Color(0xFF9E9E9E);
+  // constants of screens
+  static const kLoginView = '/';
+  static const kForgetPasswordView = '/forgetPassword';
+  static const kHomeView = '/home';
+}
+
+class AppColors {
+  static const Color blackColor = Colors.black;
+  static const Color greyColor = Colors.grey;
+  static const Color whiteColor = Colors.white;
+}
+
+class AppFonts {
+  static const kCairoFont = "Cairo";
+  static const kMontseratFont = "Montserat";
 }
