19041ce10fc9727efaa96e8e7dfc59463dcb2007
Elsp3aweyDev <elspaey9@gmail.com>
2026-04-29 23:09:01 +0300

    make app bar in home page

---
+++ lib/features/home/widgets/home_app_bar.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:my_wallet/core/constants/constants.dart';
+import 'package:my_wallet/core/constants/styles.dart';
+
+class HomeAppBar extends StatelessWidget {
+  const HomeAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+      children: [
+        IconButton(
+          style: IconButton.styleFrom(
+            padding: EdgeInsets.zero, // التحكم في الـ Padding هنا
+            minimumSize: Size.zero, // إلغاء الحد الأدنى للحجم
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          ),
+          onPressed: () {},
+
+          icon: Icon(Icons.menu, size: Constants.kSizeOfIcons),
+        ),
+        Text("My Wallet", style: Styles.textStyleBold20),
+
+        IconButton(
+          style: IconButton.styleFrom(
+            padding: EdgeInsets.zero,
+            minimumSize: Size.zero,
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          ),
+          onPressed: () {},
+          icon: Icon(Icons.notifications, size: Constants.kSizeOfIcons),
+        ),
+      ],
+    );
+  }
+}
