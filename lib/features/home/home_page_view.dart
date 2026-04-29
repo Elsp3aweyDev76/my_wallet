4c13bd77d137af523a99671deea24d2bb8d257d2
Elsp3aweyDev <elspaey9@gmail.com>
2026-04-29 23:09:18 +0300

    main home page

--- lib/features/home/home_page_view.dart
+++ lib/features/home/home_page_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:my_wallet/features/home/home_page_view_body.dart';
-import 'package:my_wallet/features/home/widgets/build_drawer.dart';
 
 class HomePageView extends StatelessWidget {
   const HomePageView({super.key});
@@ -8,9 +7,9 @@ class HomePageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
+      // appBar: AppBar(title: const Text('Home')),
       body: HomePageViewBody(),
-      drawer: BuildDrawer(),
+      // drawer: BuildDrawer(),
     );
   }
 }
