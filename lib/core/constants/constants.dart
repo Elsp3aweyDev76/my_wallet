e3823871c59caf0c80267af7f6c10d0e431997c8
Elsp3aweyDev <elspaey9@gmail.com>
2026-04-29 22:28:16 +0300

    some setting in constants

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,22 +1,12 @@
 import 'package:flutter/material.dart';
 
-const knumberOfPadding = 16.0;
-
 class Constants {
   // constants of colors
-  static const knumberOfPadding = 20.0;
+  static const knumberOfPadding = 16.0;
+  static const kSizeOfIcons = 30.0;
 
   // constants of colors
 
-  static const Color blackColor = Colors.black;
-  static const Color greyColor = Colors.grey;
-  static const Color kPrimaryOrange = Color(0xFFFF6B2C);
-  static const Color kredColor = Colors.red;
-  static const Color kAccentYellow = Color(0xFFFDBB2D);
-  static const Color kPureBlackBG = Color(0xFF000000);
-  static const Color kCardNavy = Color(0xFF121212);
-  static const Color kPureWhite = Color(0xFFFFFFFF);
-  static const Color kTextSecondary = Color(0xFF9E9E9E);
   // constants of screens
   static const kLoginView = '/';
   static const kForgetPasswordView = '/forgetPassword';
@@ -24,9 +14,17 @@ class Constants {
 }
 
 class AppColors {
-  static const Color blackColor = Colors.black;
-  static const Color greyColor = Colors.grey;
-  static const Color whiteColor = Colors.white;
+  static const Color kblackColor = Colors.black;
+  static const Color kgreyColor = Colors.grey;
+  static const Color kwhiteColor = Colors.white;
+  static const Color kPrimaryOrange = Color(0xFFFF6B2C);
+  static const Color kredColor = Colors.red;
+  static const Color kAccentYellow = Color(0xFFFDBB2D);
+  static const Color kPureBlackBG = Color(0xFF000000);
+  static const Color kCardNavy = Color(0xFF121212);
+  static const Color kPureWhite = Color(0xFFFFFFFF);
+  static const Color kTextSecondary = Color(0xFF9E9E9E);
+  static const Color kCayanColor = Colors.cyan;
 }
 
 class AppFonts {
