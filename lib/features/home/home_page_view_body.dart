1ef05002beecc48c9a9c176520476d6001387fbc
Elsp3aweyDev <elspaey9@gmail.com>
2026-06-01 02:03:54 +0300

    some info in home

--- lib/features/home/home_page_view_body.dart
+++ lib/features/home/home_page_view_body.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_wallet/core/constants/constants.dart';
-import 'package:my_wallet/core/constants/styles.dart';
-import 'package:my_wallet/core/services/function_of_space.dart';
-import 'package:my_wallet/features/home/widgets/home_app_bar.dart';
+import 'package:my_wallet/features/home/builder%20in%20home%20page/body_oh_home.dart';
+import 'package:my_wallet/features/home/builder%20in%20home%20page/color_of_backgroud.dart';
 
 class HomePageViewBody extends StatelessWidget {
   const HomePageViewBody({super.key});
@@ -11,71 +9,10 @@ class HomePageViewBody extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: double.infinity,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          stops: [0.0, 0.5],
-          colors: [AppColors.kredColor, AppColors.kCardNavy],
-        ),
-      ),
-      child: SafeArea(
-        child: Padding(
-          // padding: const EdgeInsets.symmetric(horizontal: knumberOfPadding),
-          padding: kTabLabelPadding,
-          child: Column(
-            children: [
-              verticalSpace(height: 20),
-              HomeAppBar(),
-              verticalSpace(height: 30),
-              Container(
-                // color: AppColors.kCardNavy,
-                decoration: BoxDecoration(
-                  color: AppColors.kCardNavy,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                padding: EdgeInsets.symmetric(
-                  vertical: Constants.knumberOfPadding,
-                  horizontal: Constants.knumberOfPadding,
-                ),
-                height: 100,
-                width: double.infinity,
-                // color: AppColors.kCardNavy,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      children: [
-                        Text(
-                          "the Balance",
-                          style: Styles.textStyleBold20.copyWith(
-                            fontFamily: AppFonts.kCairoFont,
-                          ),
-                        ),
-                        Text(
-                          "1000.EGP",
-                          style: Styles.textStyleRegular20.copyWith(
-                            fontFamily: AppFonts.kCairoFont,
-                            fontSize: 25,
-                          ),
-                        ),
-                      ],
-                    ),
-                    CircleAvatar(
-                      radius: 30,
-                      backgroundColor: AppColors.kCayanColor,
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.add, size: Constants.kSizeOfIcons),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      decoration: BoxDecoration(gradient: colorOfBackgroud()),
+      child: buildBodyOfHome(),
     );
   }
+
+
 }
