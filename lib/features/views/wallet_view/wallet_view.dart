bcbb4cd2d78a84cc24772f1870a98e9371213ace
Elsp3aweyDev <elspaey9@gmail.com>
2026-06-06 23:43:12 +0300

    make wallet view

--- lib/features/views/wallet_view/wallet_view.dart
+++ lib/features/views/wallet_view/wallet_view.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:my_wallet/features/views/home/builder%20in%20home%20page/color_of_backgroud.dart';
+import 'package:my_wallet/features/views/wallet_view/widgets/flow_cahrt.dart';
 
 class WalletView extends StatelessWidget {
   const WalletView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(child: Text("wallet view"));
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(gradient: colorOfBackgroud()),
+          child: Center(child: BarChartSample2()),
+          // body: SafeArea(child: Text("wallet view")),
+        ),
+      ),
+    );
   }
 }
